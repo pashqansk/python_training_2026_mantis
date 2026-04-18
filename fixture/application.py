4068306adcbc216ee9e89af7924dab0880c2051f
Тимошенко Павел <timoshenko@signatec.ru>
2026-04-18 20:22:25 +0700

    + Telnet

--- fixture/application.py
+++ fixture/application.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
 
 
 class Application:
 
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -16,8 +17,10 @@ class Application:
         else:
             raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
-        self.base_url = base_url
+        self.james = JamesHelper(self)
+        self.config = config
         self.project = ProjectHelper(self)
+        self.base_url = config['web']['base_url']
 
 
     def is_valid(self):
